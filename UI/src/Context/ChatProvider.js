cfad4326e256af748cc760ba2a193a7038a3b5ee
novasultana <nova.sultana20@gmail.com>
2023-06-01 17:31:10 +0600

    hlw....

--- UI/src/Context/ChatProvider.js
+++ UI/src/Context/ChatProvider.js
@@ -12,6 +12,7 @@ const ChatProvider = ({ children }) => {
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    console.log(userInfo);
     setUser(userInfo);
 
     if (!userInfo) {
